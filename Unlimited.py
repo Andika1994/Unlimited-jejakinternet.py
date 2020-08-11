03e6ddf732f3c0fe81a4aa25a1e0ae9eb1bff4c5
hadapan <69392219+hadapan@users.noreply.github.com>
2020-08-11 15:46:34 +0700

    Create Unlimited.py

---
+++ Unlimited.py
@@ -0,0 +1,72 @@
+#Coded By Zidan-ID\King Mr_Z17
+
+#YouTube Mr_Z17
+
+#Vortex Indonesia
+
+import os,sys,time
+
+ # SILAHKAN COSTUMISASI SENDIRI YA BOSQ #
+
+def main():
+
+    time.sleep(1)
+
+    os.system ('clear')
+
+    print '[96m'
+
+    os.system ('figlet Spam Unlimited')
+
+    print'[92m================================='
+
+    print'[97m Author : [96mJejak Internet '
+
+    print'[97m YouTube   : [96mJejak Internet '
+
+    print'[92m================================='
+
+    print'[92m++++++++++++ [97mM E N U [92m++++++++++++'
+
+    print'[92m[[97m1[92m] [97mSpam Unlimited'
+
+    print'[92m[[91m0[92m] [91mExit '
+
+    gans = raw_input ('[97m==>[93m ')
+
+    if gans in ['1']:
+
+        time.sleep(1)
+
+        os.system ('git clone https://github.com/MainIT/Tools-Spam2k')
+
+        print
+
+        print (' {+} Proses Instalasi Selesai')
+
+        print (' {+} Silakan Ketik cd Tools-Spam2k')
+
+        print (' {+} Habis Itu Ketik ls')
+
+        print (' {+} Jikas Sudah Silakan Ketik python2 update_v2.py')
+
+        exit()
+
+    if gans in ['0']:
+
+        time.sleep(2)
+
+        exit()
+
+    else:
+
+        time.sleep(1)
+
+        print '[97m Pilih Yang Bener [92mBro . . .'
+
+        time.sleep(1)
+
+        main()
+
+main()
+
